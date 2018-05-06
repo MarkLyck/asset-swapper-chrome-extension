90979380b1eaef36d4ce9dd9ff6eefc680ebb2b7
MarkLyck <mark.lyck@gmail.com>
2018-05-06 22:17:07 +0200

    commit dist folder

---
+++ dist/background.js
@@ -0,0 +1,51 @@
+/*
+ * Created by Mark Lyck
+ * mark.lyck@gmail.com
+ * Licence GPL3
+ */
+
+// Check if the version has changed.
+var currVersion = getVersion();
+var prevVersion = localStorage['version'];
+if (currVersion != prevVersion) {
+	// Check if we just installed this extension.
+	if (typeof prevVersion == 'undefined') {
+		onInstall();
+	}
+	localStorage['version'] = currVersion;
+}
+
+/**
+ * RegExp-escapes all characters in the given string.
+ */
+function regExpEscape (s) {
+	return s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+}
+  
+function wildcardToRegExp (s) {
+	return new RegExp('^' + s.split(/\*+/).map(regExpEscape).join('.*') + '$')
+}
+
+chrome.webRequest.onBeforeRequest.addListener(details => {
+	var rules = JSON.parse(localStorage.rules)
+	return rules.reduce((acc, rule) => {
+		const sourceRegex = wildcardToRegExp(rule.source)
+		if (rule.active && sourceRegex.test(details.url)) {
+			acc = { redirectUrl: rule.target }
+		}
+		return acc
+	}, {})
+}, {
+	urls: ["<all_urls>"]
+}, ["blocking"])
+
+function onInstall() {
+	if (typeof localStorage.configuration === "undefined") {
+		localStorage.rules = "[]";
+	}
+}
+
+function getVersion() {
+	var details = chrome.app.getDetails()
+	return details.version
+}
